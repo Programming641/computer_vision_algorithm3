5a81510f4ea5db7f60edabad941514d19b001972
Programming641 <56218301+Programming641@users.noreply.github.com>
2022-03-30 17:40:34 +0900

    Add files via upload

---
+++ findimage_pixelchange.py
@@ -0,0 +1,76 @@
+
+
+from PIL import Image
+import re
+
+import os, sys
+
+
+directory = "videos/hanger/"
+im1file = "hanger001_color_group"
+im2file = "hanger002_color_group"
+
+# directory is specified but does not contain /
+if directory != "" and directory[-1] != ('/'):
+   directory +='/'
+
+
+
+
+im1 = Image.open("images/" + directory + im1file + ".png" )
+im1pxls = im1.getdata()
+im1width, im1height = im1.size
+
+im2 = Image.open("images/" + directory + im2file + ".png" )
+im2pxls = im2.getdata()
+im2width, im2height = im2.size
+
+
+
+
+print("im1width " + str(im1width) + " im1height " + str(im1height) )
+
+pxlch_im = Image.new('RGB', (im1width,im1height))
+
+newdata = []
+
+
+debug = False
+
+
+#image_size[0] is image width
+for y in range(im1height):
+  print("y is " + str(y))
+
+
+  for x in range(im1width):
+
+    #getting pixel index value.
+    pixel_index =  ( y * im1width ) + x
+    
+    if im1pxls[pixel_index] != im2pxls[pixel_index] and ( im1pxls[pixel_index] != (255, 255, 255) and im2pxls[pixel_index] != (255, 255, 255) ) :
+       print("current pixel changed! which is located at x " + str(x) )
+       print(im2pxls[pixel_index])
+       
+       newdata.append( im2pxls[pixel_index] )
+
+    else:
+       newdata.append( (255, 255, 255) )    
+    
+  if debug == True:
+       break
+    
+  if debug == True:
+       break
+
+
+pxlch_im.putdata (newdata)
+pxlch_im.save ("images/" + directory + "diff12result2.png")
+
+
+
+
+
+
+
+
