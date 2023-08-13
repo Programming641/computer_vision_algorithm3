2c1b8466f118750481a20f3a6ff0cee20d486d96
Programming641 <56218301+Programming641@users.noreply.github.com>
2023-08-13 09:38:40 +0900

    Add files via upload

---
+++ algorithms/fundamental/find_shapes.py
@@ -0,0 +1,175 @@
+#
+# algorithm for determining the closest match with the color group uses how far away the RGB value is from original
+#
+#
+from libraries import pixel_functions
+
+from PIL import Image
+import math
+import os, sys
+import winsound
+
+from collections import OrderedDict
+from libraries.cv_globals import top_shapes_dir, top_images_dir
+
+
+image_filename = '2'
+directory = "tests/min"
+
+if len(sys.argv) >= 2:
+   image_filename = sys.argv[0][0: len( sys.argv[0] ) - 4 ]
+
+   directory = sys.argv[1]
+
+   print("execute script find_shapes.py. filename " + image_filename + " directory " + directory )
+
+
+# directory is specified but does not contain /
+if directory != "" and directory[-1] != '/':
+   directory +='/'
+
+target_im_shapes_dir = top_shapes_dir + directory + "shapes/"
+
+
+if os.path.exists(target_im_shapes_dir ) == False:
+   os.makedirs(target_im_shapes_dir)
+   
+   
+
+original_image = Image.open(top_images_dir + directory + image_filename + ".png")
+original_pixel = original_image.getdata()
+image_size = original_image.size
+
+
+# shapes[shape's index value ] = [ pixel index1, pixel index2, .... ]
+shapes = OrderedDict()
+
+
+#image_size[0] is width
+for y in range(image_size[1]):
+   print("y is " + str(y))
+
+
+   for x in range(image_size[0]):
+   
+   
+      # converting for the getdata(). y is the row that you want to get data for. current_pixel_index is the current pixel's index number
+      current_pixel_index = (y * image_size[0])+ x    
+
+      # initializing current pixel' shape number. This 
+      current_shape_id = None
+      
+      # this is needed for checking if pixel has same color with every pixels in the confirmed shapes
+      cur_shape_containing_confirmed_shapes = None
+      
+      cur_pixel_neighbors = pixel_functions.get_nbr_pixels(current_pixel_index, image_size)      
+
+      # check if current pixel is in other pixel's shapes.
+      if len(shapes) != 0:
+         # last in first out
+         for shape_id, shapes_lists in reversed(shapes.items()):
+            if current_pixel_index in shapes_lists:
+               # current pixel is found in existing shape. current shape will be this found shape
+               current_shape_id = shape_id
+               
+               cur_shape_containing_confirmed_shapes = shapes_lists
+
+
+      # current pixel was not found in all shapes so create its own shape
+      if current_shape_id == None:     
+         # create shape with current pixel's number as shape's id
+         current_shape_id = current_pixel_index
+         # make sure to put current pixel in current pixel shape
+         
+         shapes[current_shape_id] = [current_shape_id]
+
+
+      cur_shape_pixel_RGB = original_pixel[current_shape_id]
+
+
+
+      # iterating each one of current pixel's unfound neighbors
+      for neighbor in cur_pixel_neighbors:
+         
+         neighbor_shape_found, cur_shape_nei_shape_appearance = False, False
+         neighbor_shape_index = None  
+         
+         cur_nbr_containing_shapes = []
+         # check if this neighbor is already in the shape
+         for shape_id, shapes_lists in reversed(shapes.items()):
+            # iterating all neighbor shapes
+            if neighbor in shapes_lists:
+                  
+                  # check if neighbor is already in the same shape as current_pixel_index
+                  if shape_id == current_shape_id:
+                     neighbor_shape_found = True 
+                     break
+                  
+                  cur_nbr_containing_shapes = shapes_lists
+                               
+                  neighbor_shape_index = shape_id                
+                              
+
+
+         if cur_nbr_containing_shapes:
+            # neighbor is found in neighbor_shape_index
+            # check if all pixels in the  neighbor_shape_index have same same as current_shape_id
+            # merge two shapes only if they all have the same color
+            # merging all pixels in neighbor shape into current shape
+
+            all_pixels_same_clr = None
+            for cur_nbr_containing_pixel in cur_nbr_containing_shapes:
+               for cur_shape_pixel in shapes[current_shape_id]:
+               
+                  clrch, brit_thres, brit_ch_v = pixel_functions.compute_appearance_difference(original_pixel[cur_nbr_containing_pixel] ,
+                                                 original_pixel[cur_shape_pixel], 30 )
+                  
+                  # color changed is true or color did not change but brightness is not within threshold
+                  if clrch or ( not clrch and not brit_thres ):
+                     all_pixels_same_clr = False
+                     break
+
+               if all_pixels_same_clr == False:
+                  break
+                  
+            if all_pixels_same_clr != False:
+               shapes[current_shape_id].extend(shapes[neighbor_shape_index])
+               
+               shapes.pop(neighbor_shape_index)       
+
+
+         if  neighbor_shape_found == False:
+
+            neighbor_pixel_RGB = original_pixel[neighbor]
+
+            # make sure all pixels have same color as neighbor, then you can put neighbor in current_shape_id shape
+            all_pixels_same_clr = None
+            if cur_shape_containing_confirmed_shapes:
+               for pixels in cur_shape_containing_confirmed_shapes:
+                  clrch, brit_thres, brit_ch_v = pixel_functions.compute_appearance_difference(original_pixel[pixels], neighbor_pixel_RGB, 30 )
+                 
+                  if clrch or ( not clrch and not brit_thres ):
+                     all_pixels_same_clr = False
+                     break
+               
+               if all_pixels_same_clr != False:
+                  
+                  # neighbor pixel will be added to current shape
+                  shapes[current_shape_id].append(neighbor)                 
+            
+
+
+file = open(target_im_shapes_dir + image_filename + "_shapes.txt", 'w')
+file.write(str(shapes))
+file.close()
+
+
+frequency = 2500  # Set Frequency To 2500 Hertz
+duration = 1000  # Set Duration To 1000 ms == 1 second
+winsound.Beep(frequency, duration)
+
+
+
+
+
+
---
+++ algorithms/fundamental/putpix_into_clrgrp.py
@@ -0,0 +1,163 @@
+#
+# algorithm for determining the closest match with the color group uses how far away the RGB value is from original
+#
+#
+
+import sys, os
+from PIL import Image
+import re
+import winsound
+from libraries import pixel_functions
+from libraries.cv_globals import proj_dir
+
+if proj_dir != "" and proj_dir[-1] != "/":
+   proj_dir +='/'
+
+image_dir = proj_dir + "images/"
+
+# -------------------       user input begin     ---------------------
+# user input: filename, directory ( example. videos/cat/original ), 
+filename = "25"
+directory = "videos/street3/resized/"
+# types of color groups. choices are clrgrp ( for color group ), min ( for minimum )
+clr_grp_type = "min"
+# -------------------       user input end       ---------------------
+
+
+
+
+
+if len(sys.argv) >= 2:
+   filename = sys.argv[0][0: len( sys.argv[0] ) - 4 ]
+   directory = sys.argv[1]
+   clr_grp_type = sys.argv[2]
+
+   print("execute script putpix_into_clrgrp.py. filename " + filename + " directory " + directory + " clr_grp_type " + clr_grp_type )
+
+
+
+# directory is specified but does not contain /
+if directory != "" and directory[-1] != "/":
+   directory +='/'
+
+
+original_image = Image.open(image_dir + directory + filename + ".png")
+original_pixel = original_image.getdata()
+image_size = original_image.size
+
+debug = 10
+
+def get_closest_color(image_pixel):
+
+    global debug
+
+    if clr_grp_type == "clrgrp":
+       file = open(proj_dir + "data/list of colors3.txt")
+    elif clr_grp_type == "min":
+       file = open(proj_dir + "data/minimum_colors.txt")
+    
+    lines = file.readlines()
+
+    #dictionary for storing color group name and rgb values. this is needed after finding the image pixel's closest color group
+    color_group_dict = {}
+
+
+
+    # initializing dictionary for storing total difference value for every matching pixel with particular color group.
+    difference_dict = {}
+
+    for line in lines: 
+
+       hex_pattern = '#[a-zA-Z0-9]{6}'
+       hex_Num = re.search( hex_pattern , line)
+       
+       if not hex_Num:
+          continue
+
+       
+       hex_rm = hex_Num.group().replace("#", "")
+       
+       red_color_group = int( hex_rm[0:2], 16 )
+       green_color_group = int( hex_rm[2:4], 16 )
+       blue_color_group = int( hex_rm[4:6], 16 )
+           
+       if len(image_pixel) == 3:
+          image_red, image_green, image_blue = image_pixel
+       else:
+          image_red, image_green, image_blue, alpha = image_pixel
+         
+       red_difference = abs(image_red - red_color_group)
+       green_difference = abs(image_green - green_color_group)
+       blue_difference = abs(image_blue - blue_color_group)
+            
+       total_difference = red_difference + green_difference + blue_difference
+
+       average = total_difference / 3
+
+       # exclude 0 or negatie values because 0 or negative values mean that difference is smaller than average.
+       red_how_far = exclude_negative_Num( red_difference - average )
+       green_how_far = exclude_negative_Num( green_difference - average )
+       blue_how_far = exclude_negative_Num( blue_difference - average )
+
+       total_appearance_difference = total_difference + red_how_far * 1.3 + green_how_far * 1.3 + blue_how_far * 1.3
+
+       #populating all color group hex number and values
+       color_group_dict[hex_Num] = [red_color_group, green_color_group, blue_color_group]
+
+       difference_dict[hex_Num] = total_appearance_difference
+
+    # key_min has the color group hex number that has the closest match for the image pixel
+    key_min = min(difference_dict, key=difference_dict.get)
+
+    return color_group_dict[key_min]
+        
+    file.close()    
+
+
+def exclude_negative_Num(num):
+
+    if num > 0:
+       return num
+    else:
+       return 0
+
+
+
+#image_size[0] is image width
+for y in range(image_size[1]):
+   print("y is " + str(y))
+
+   for x in range(image_size[0]):
+   
+
+
+        # converting for the getdata(). y is the row that you want to get data for
+        new_x = (y * image_size[0])+ x
+
+        red, green, blue = get_closest_color(original_pixel[new_x])
+
+        original_image.putpixel( (x,y), (red, green, blue))
+        
+
+save_dirname = image_dir + directory + clr_grp_type
+if not os.path.exists(save_dirname):
+   os.makedirs(save_dirname)
+
+
+original_image.save(save_dirname + "/" + filename + ".png")
+
+
+
+
+frequency = 2500  # Set Frequency To 2500 Hertz
+duration = 1000  # Set Duration To 1000 ms == 1 second
+winsound.Beep(frequency, duration)
+
+
+
+
+
+
+
+
+
